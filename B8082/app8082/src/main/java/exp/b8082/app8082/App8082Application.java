3658da7d49900e767abca366fd9f3d997f4f2d31
Aression <1040901628@qq.com>
2021-10-12 16:38:48 +0800

    initialize project files

---
+++ B8082/app8082/src/main/java/exp/b8082/app8082/App8082Application.java
@@ -0,0 +1,13 @@
+package exp.b8082.app8082;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App8082Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App8082Application.class, args);
+	}
+
+}
