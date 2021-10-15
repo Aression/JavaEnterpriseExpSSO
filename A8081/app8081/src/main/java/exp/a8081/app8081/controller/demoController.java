dc0d651a630a4e55e42bb87dd27c1caa10642f67
Aression <57360540+Aression@users.noreply.github.com>
2021-10-15 17:48:21 +0800

    update System A

--- A8081/app8081/src/main/java/exp/a8081/app8081/controller/demoController.java
+++ A8081/app8081/src/main/java/exp/a8081/app8081/controller/demoController.java
@@ -1,12 +1,19 @@
 package exp.a8081.app8081.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class demoController {
     @RequestMapping("/demo")
-    public String demo(){
+    public String demo(Model model){
+        model.addAttribute("name", "testData!");
+        
         return "demo";
     }
+     
 }
